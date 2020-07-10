191e0b7ce741516a202ed1c42ed0ea3b001db803
daniel-qwerty <daniel.monroy.b@gmail.com>
2020-07-09 21:51:24 -0300

    correccion asociados

--- Modules/Teams/Widget/Team.php
+++ Modules/Teams/Widget/Team.php
@@ -46,7 +46,7 @@ class Teams_Widget_Team extends Com_Object
                     <div class="gdlr-core-personnel-list-image gdlr-core-media-image  gdlr-core-opacity-on-hover gdlr-core-zoom-on-hover gdlr-core-grayscale-effect">
                         <a href="<?=Com_Helper_Url::getInstance()->urlBase . '/' . $this->lan->LanCode . '/perfil/' . $item->TeamId?>"><img src="<?PHP echo Com_Helper_Url::getInstance()->getUploads(); ?>/Image/<?=$item->TeamThumb?>" alt="<?=$item->TeamNombre?>" width=500 height=500 title="<?=$item->TeamNombre?>"></a>
                     </div>
-                    <div class=gdlr-core-personnel-list-content-wrap style="margin-top: 10px;">
+                    <div class=gdlr-core-personnel-list-content-wrap style="margin-top: 10px; background:white;padding:10px">
                         <h3 class="gdlr-core-personnel-list-title" style="font-size: 16px ;font-weight: 600 ;letter-spacing: 2px ;margin-bottom: 5px ;"><a href="<?=Com_Helper_Url::getInstance()->urlBase . '/' . $this->lan->LanCode . '/perfil/' . $item->TeamId?>" ><?=$item->TeamNombre?></a></h3>
                         <div class="gdlr-core-personnel-list-position gdlr-core-info-font gdlr-core-skin-caption" style="font-size: 13px ;font-weight: 500 ;font-style: normal ;letter-spacing: 2px ;text-transform: uppercase ;"><?=$item->TeamCargo?></div>
                     </div>
