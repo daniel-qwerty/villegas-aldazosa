7e4ab7db2f9ceb27a509574ca1b667002f4ee014
Daniel Monroy <daniel.monroy.b@gmail.com>
2020-07-16 13:24:10 -0400

    correccion en asociados

--- Modules/Teams/Widget/TeamList.php
+++ Modules/Teams/Widget/TeamList.php
@@ -1,6 +1,6 @@
 <?php
 
-class Teams_Widget_Team extends Com_Object
+class Teams_Widget_TeamList extends Com_Object
 {
 
     private $lan;
@@ -10,7 +10,7 @@ class Teams_Widget_Team extends Com_Object
      *
      * @static
      * @access public
-     * @return Teams_Widget_Team
+     * @return Teams_Widget_TeamList
      */
     public static function getInstance()
     {
@@ -40,20 +40,32 @@ class Teams_Widget_Team extends Com_Object
         foreach ($list as $item) {
 
             ?>
-                <div class="gdlr-core-personnel-list gdlr-core-outer-frame-element clearfix" style="box-shadow: 0 0 35px rgba(10, 10, 10,0.08); -moz-box-shadow: 0 0 35px rgba(10, 10, 10,0.08); -webkit-box-shadow: 0 0 35px rgba(10, 10, 10,0.08); border-radius: 4px;-moz-border-radius: 4px;-webkit-border-radius: 4px; margin:5px;">
-                    <div class="gdlr-core-personnel-list-image gdlr-core-media-image  gdlr-core-opacity-on-hover gdlr-core-zoom-on-hover gdlr-core-grayscale-effect">
-                        <a href="<?=Com_Helper_Url::getInstance()->urlBase . '/' . $this->lan->LanCode . '/perfil/' . $item->TeamId?>"><img src="<?PHP echo Com_Helper_Url::getInstance()->getUploads(); ?>/Image/<?=$item->TeamThumb?>" alt="<?=$item->TeamNombre?>" width=500 height=500 title="<?=$item->TeamNombre?>"></a>
-                    </div>
-                    <div class=gdlr-core-personnel-list-content-wrap style="margin-top: 10px;">
-                        <h3 class="gdlr-core-personnel-list-title" style="font-size: 16px ;font-weight: 600 ;letter-spacing: 2px ;margin-bottom: 5px ;"><a href="<?=Com_Helper_Url::getInstance()->urlBase . '/' . $this->lan->LanCode . '/perfil/' . $item->TeamId?>" ><?=$item->TeamNombre?></a></h3>
-                        <div class="gdlr-core-personnel-list-position gdlr-core-info-font gdlr-core-skin-caption" style="font-size: 13px ;font-weight: 500 ;font-style: normal ;letter-spacing: 2px ;text-transform: uppercase ;"><?=$item->TeamCargo?></div>
-                    </div>
-                </div>
 
 
+<div class="gdlr-core-personnel-list-column  gdlr-core-column-20 gdlr-core-item-pdlr">
+    <div class="gdlr-core-personnel-list gdlr-core-outer-frame-element clearfix"
+        style="box-shadow: 0 0 45px rgba(10, 10, 10,0.08); -moz-box-shadow: 0 0 45px rgba(10, 10, 10,0.08); -webkit-box-shadow: 0 0 45px rgba(10, 10, 10,0.08); border-radius: 4px;-moz-border-radius: 4px;-webkit-border-radius: 4px;">
+        <div
+            class="gdlr-core-personnel-list-image gdlr-core-media-image  gdlr-core-opacity-on-hover gdlr-core-zoom-on-hover gdlr-core-grayscale-effect">
+            <a
+                href="<?=Com_Helper_Url::getInstance()->urlBase . '/' . $this->lan->LanCode . '/perfil/' . $item->TeamId?>"><img
+                    src="<?PHP echo Com_Helper_Url::getInstance()->getUploads(); ?>/Image/<?=$item->TeamThumb?>" alt
+                    width=599 height=551 title=Emma2></a>
+        </div>
+        <div class=gdlr-core-personnel-list-content-wrap>
+            <h3 class="gdlr-core-personnel-list-title"
+                style="font-size: 16px ;font-weight: 600 ;letter-spacing: 3px ;margin-bottom: 4px ;"><a
+                    href="<?=Com_Helper_Url::getInstance()->urlBase . '/' . $this->lan->LanCode . '/perfil/' . $item->TeamId?>"><?=$item->TeamNombre?></a>
+            </h3>
+            <div class="gdlr-core-personnel-list-position gdlr-core-info-font gdlr-core-skin-caption"
+                style="font-size: 13px ;font-weight: 500 ;font-style: normal ;letter-spacing: 2px ;text-transform: uppercase ;">
+                <?=$item->TeamCargo?></div>
 
+        </div>
+    </div>
+</div>
 
-            <?PHP
+<?PHP
 
         }
     }
